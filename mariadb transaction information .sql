5a0c2f9b3e1c0ee073866e13c59f8ffa456501bd
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2023-12-13 11:26:10 +0530

    Update mariadb transaction information .sql

--- mariadb transaction information .sql
+++ mariadb transaction information .sql
@@ -1,5 +1,3 @@
-
-
 show processlist;  -- to see the all ongoing processes in mariadb
 select * from information_schema.innodb_trx;  -- to get Information about sql transactions 
 SHOW OPEN TABLES WHERE In_use > 0; -- to get information about the tables that are in use 
